bb2d702bc142515ebf7ebc6c2bf40ae5165c2d2c
mssandeepkamath <msandeepcip@gmail.com>
2022-12-09 15:05:12 +0530

    first_commit

---
+++ src/main/java/com/workspace/management/restfulapi_workspace_management/ResTfulApiWorkspaceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.workspace.management.restfulapi_workspace_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulApiWorkspaceManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResTfulApiWorkspaceManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/workspace/management/restfulapi_workspace_management/ResTfulApiWorkspaceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workspace.management.restfulapi_workspace_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulApiWorkspaceManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
