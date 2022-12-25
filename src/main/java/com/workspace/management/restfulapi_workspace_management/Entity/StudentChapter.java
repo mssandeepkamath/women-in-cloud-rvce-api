92ac66b571c086670e164475829e4bf20c9b2322
nisarga3 <ms.nisarga3@gmail.com>
2022-12-25 20:41:21 +0530

    attributes added to entities

--- src/main/java/com/workspace/management/restfulapi_workspace_management/Entity/StudentChapter.java
+++ src/main/java/com/workspace/management/restfulapi_workspace_management/Entity/StudentChapter.java
@@ -1,4 +1,49 @@
 package com.workspace.management.restfulapi_workspace_management.Entity;
 
 public class StudentChapter {
+    String President;
+    String Vice_President;
+    String Treasurer;
+    String faculty;
+    String year; //year of student chapter
+
+    public String getPresident() {
+        return President;
+    }
+
+    public void setPresident(String president) {
+        President = president;
+    }
+
+    public String getVice_President() {
+        return Vice_President;
+    }
+
+    public void setVice_President(String vice_President) {
+        Vice_President = vice_President;
+    }
+
+    public String getTreasurer() {
+        return Treasurer;
+    }
+
+    public void setTreasurer(String treasurer) {
+        Treasurer = treasurer;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
 }
